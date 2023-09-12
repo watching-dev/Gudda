de081c0b951edb0a91db658191c85eb8cfbc3b9f
watching-dev <coderefactoring90@gmail.com>
2023-09-12 15:49:55 +0900

    [init] 걷다 프로젝트 생성 #1

---
+++ .github/ISSUE_TEMPLATE/iOS_ISSUE_TEMPLATE.md
@@ -0,0 +1,15 @@
+---
+name: Gudda Issue Template
+about: '걷다 이슈 템플릿'
+title: '[]'
+labels: ''
+assignees: ''
+---
+
+### 이슈 설명
+
+
+
+### 체크 리스트
+
+- [ ] 
---
+++ Gudda/Gudda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gudda
+//
+//  Created by bro on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
